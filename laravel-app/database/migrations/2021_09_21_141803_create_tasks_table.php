9d939b13f1f44c33765c9f78aab30866dd8876cb
mehdihasanpour <mehdih761@yahoo.com>
2021-09-21 22:44:59 +0430

    modified task migration and env file and create one database in phpmyadmin

--- laravel-app/database/migrations/2021_09_21_141803_create_tasks_table.php
+++ laravel-app/database/migrations/2021_09_21_141803_create_tasks_table.php
@@ -15,6 +15,10 @@ class CreateTasksTable extends Migration
     {
         Schema::create('tasks', function (Blueprint $table) {
             $table->id();
+            $table->bigIncrements('parent_id')->nullable();
+            $table->string('title');
+            $table->boolean('completed')->default(false);
+            $table->timestamp('scheduled_for');
             $table->timestamps();
         });
     }
